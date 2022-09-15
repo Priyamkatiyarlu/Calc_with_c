2cedb857f65a8ad5de71b45dfb10acded03300a5
Priyamkatiyarlu <104723191+Priyamkatiyarlu@users.noreply.github.com>
2022-09-15 22:10:55 +0530

    pointer removed
    Removed pointer and have a doubt on memory allocation with pointer.

--- matrixincomplete.c
+++ matrixincomplete.c
@@ -1,87 +1,212 @@
 #include<stdio.h>
-int printresult1(int x1,int y1,int arr1[x1][y1]){
-        for(int i=0; i<x1; i++){
+int x1,y1,x2,y2,x3,y3;
+int arr1[100][100];
+int arr2[100][100];
+int arr3[100][100];
+int res[100][100];
+int printresult(int x,int y,int arr[100][100]){
+        for(int i=0; i<x; i++){
         printf("\n");
-        for(int j=0; j<y1; j++)
-        printf("%d\t",arr1[i][j]);
+        for(int j=0; j<y; j++)
+        printf("%d\t",arr[i][j]);
         }
         
     }
-int matrixrc1(int *x1,int *y1){
+int matrixrc(int *x1,int *y1){
     printf("Enter no of rows=");
     scanf("%d",&*x1);
     printf("Enter no of columns=");
     scanf("%d",&*y1);
 }
-int matrixrc2(int *x2,int *y2){
-    printf("Enter no of rows=");
-    scanf("%d",&*x2);
-    printf("Enter no of columns=");
-    scanf("%d",&*y2);
-}
-int matrixrc3(int *x3,int *y3){
-    printf("Enter no of rows=");
-    scanf("%d",&*x3);
-    printf("Enter no of columns=");
-    scanf("%d",&*y3);
-}
-   int matrixinput1(int x1,int y1,int arr1[x1][y1]){
-    for(int i=0; i<x1; i++){
-        for(int j=0; j<y1; j++){
+   int matrixinput(int x,int y,int arr[100][100]){
+    for(int i=0; i<x; i++){
+        for(int j=0; j<y; j++){
         printf("MatA[%d][%d]:\t",i+1,j+1);
-        scanf("%d",&arr1[i][j]);
+        scanf("%d",&arr[i][j]);
         }
     }
     }
-    int matrixinput2(int x2,int y2,int arr2[x2][y2]){
-    for(int i=0; i<x2; i++){
-        for(int j=0; j<y2; j++){
-        printf("MatA[%d][%d]:\t",i+1,j+1);
-        scanf("%d",&arr2[i][j]);
-        }
-    }
-    }
-    int matrixinput3(int x3,int y3,int arr3[x3][y3]){
-    for(int i=0; i<x3; i++){
-        for(int j=0; j<y3; j++){
-        printf("MatA[%d][%d]:\t",i+1,j+1);
-        scanf("%d",&arr3[i][j]);
-        }
-    }
-    }
-    int matA(){
-    int x1,y1;
-    matrixrc1(&x1,&y1);
-    int arr1[x1][y1];
-    matrixinput1(x1,y1,arr1);
-    }
-    int matB(){
-    int x2,y2;
-    matrixrc2(&x2,&y2);
-    int arr2[x2][y2];
-    matrixinput2(x2,y2,arr2);
-    }
-    int matC(){
-    int x3,y3;
-    matrixrc3(&x3,&y3);
-    int arr3[x3][y3];
-    matrixinput3(x3,y3,arr3);
-    }
-        int main(){
+        void dim(){
         printf("1. MatA\n2. MatB\n3. matC\n");
         int n;
     printf("\nEnter your choice:\t");
     scanf("%d",&n);
     switch(n){
         case 1:
-        matA();
+        matrixrc(&x1,&y1);
+        matrixinput(x1,y1,arr1);
         break;
         case 2:
-        matB();
+        matrixrc(&x2,&y2);
+        matrixinput(x2,y2,arr2);
         break;
         case 3:
-        matC();
+        matrixrc(&x3,&y3);
+        matrixinput(x3,y3,arr3);
+        break;
+        }
+        }
+    int printmatA(){
+    printresult(x1,y1,arr1);
+    }
+    int printmatB(){
+    printresult(x2,y2,arr2);
+    }
+    int printmatC(){
+    printresult(x3,y3,arr3);
+    }
+    void display(){
+        printf("1. Display MatA\n2. Display MatB\n3.Display MatC\n");
+        int n;
+    printf("\nEnter your choice:\t");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        printmatA();
+        break;
+        case 2:
+        printmatB();
+        break;
+        case 3:
+        printmatC();
         break;
 
     }
+    }
+    void transmat(int x, int y,int array[100][100]){
+    int res[x][y];
+     for(int i=0;i<x;i++){
+        for(int j=0; j<y; j++){
+                res[j][i]=array[i][j];
+            }
+        }
+    for(int i=0;i<x;i++){
+        for(int j=0; j<y; j++){
+                printf("%d\t",res[i][j]);
+            }
+            printf("\n");
+        }
+        }
+    void trans(){
+        printf("1. MatA\n2. MatB\n3. matC\n");
+        int n;
+    printf("\nEnter your choice:\t");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        transmat(x1,y1,arr1);
+        break;
+        case 2:
+        transmat(x2,y2,arr2);
+        break;
+        case 3:
+        transmat(x3,y3,arr3);
+        break;
+
+    }
+} 
+int det(int A[100][100], int n)
+{
+    int Minor[100][100];
+    int i,j,k,c1,c2;
+    int determinant;
+    int c[100];
+    int O=1;
+
+    if(n == 2)
+    {
+        determinant = 0;
+        determinant = A[0][0]*A[1][1]-A[0][1]*A[1][0];
+        return determinant;
+    }
+    else
+    {
+        for(i = 0 ; i < n ; i++)
+        {
+            c1 = 0, c2 = 0;
+            for(j = 0 ; j < n ; j++)
+            {
+                for(k = 0 ; k < n ; k++)
+                {
+                    if(j != 0 && k != i)
+                    {
+                        Minor[c1][c2] = A[j][k];
+                        c2++;
+                        if(c2>n-2)
+                        {
+                            c1++;
+                            c2=0;
+                        }
+                    }
+                }
+            }
+            determinant = determinant + O*(A[0][i]*det(Minor,n-1));
+            O=-1*O;
+        }
+    }
+    return determinant;
+}
+void detopt(){
+    printf("1. MatA\n2. MatB\n3. matC\n");
+        int n;
+    printf("\nEnter your choice:\t");
+    scanf("%d",&n);
+    int res1,res2,res3;
+    switch(n){
+        case 1:
+        if (x1==y1){
+        res1=det(arr1,x1);
+        printf("Det of MatA:\t%d",res1);
+        }
+        else{
+            printf("Mathematical Error");
+        }
+        break;
+        case 2:
+        if (x2==y2){
+        res2=det(arr2,x2);
+        printf("Det of MatB:\t%d",res2);
+        }
+        else{
+            printf("Mathematical Error");
+        }
+        break;
+        case 3:
+        if (x2==y2){
+            res3= det(arr2,x2);
+        printf("Det of MatC:\t%d",res3);
+        }
+        else{
+            printf("Mathematical Error");
+        }
+        break;
+
+    }
+}
+    int main(){
+        printf("\n");
+        printf("1. Dim\n2. Edit\n3. Mat\n4. Det\n5. Trans\n6. display\n7. Exit\n");
+        int n;
+    printf("\nEnter your choice:\t");
+    scanf("%d",&n);
+    switch(n){
+        case 1:
+        dim();
+        break;
+        case 2:
+        break;
+        case 4:
+        detopt();
+        break;
+        case 5:
+        trans();
+        break;
+        case 6:
+        display();
+        break;
+        case 7:
+        return 0;
+
+    }
+    main();
 }
