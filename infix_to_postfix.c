76d29214a0e4ac9b0290db5569df23648096ab94
Priyamkatiyarlu <104723191+Priyamkatiyarlu@users.noreply.github.com>
2023-07-02 13:17:52 +0530

    Update infix_to_postfix.c Evaluation needs to be changed as it works only on single degit
    Not working on multiple digits

--- infix_to_postfix.c
+++ infix_to_postfix.c
@@ -1,99 +1,152 @@
 //infix to postfix 
+//Programmer:-Priyam Katiyar
+//Purpose:-Evaluation of expressions for a project (Scintific calculator)
+//Data structure used:- Infix to postfix conversion
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-struct stack{
-    int size;
-    int top;
-    int *arr;
-};
-int stackTop(struct stack* sp){
-    return sp->arr[sp->top];
+#define MAX 100
+ int stack[MAX];
+ char infix[MAX], postfix[MAX];
+ int top=-1;
+ void push(int);
+ int pop();
+ int isEmpty();
+ void inToPost();
+ int space(char);
+ void print();
+ int precedence(char);
+
+ int space(char c){
+    if(c==' '||c=='\t'){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+ }
+ int precedence(char symbol){
+    switch(symbol){
+        case '^':
+        return 3;
+        case '*':
+        case '/':
+        return 2;
+        case '+':
+        case '-':
+        return 1;
+        default:
+        return 0;
+    }
+ }
+ void print(){
+    int i=0;
+    printf("The equivalent postfix expression is= ");
+    while (postfix[i]){
+        printf("%c",postfix[i++]);
+    }
+    printf("\n");
 }
-int precedence(char ch){
-    if(ch=='*'||ch=='/')
-    return 3;
-    else if(ch=='+'||ch=='-')
-    return 2;
-    else
-    return 0;
+void push(int c){
+    if(top==MAX-1){
+        printf("Stack overflow\n");
+        return;
+    }
+    top++;
+    stack[top]=c;
 }
-int isOperator(char ch){
-    if(ch=='+'||ch=='-'||ch=='*'||ch=='/')
+int pop(){
+    int c;
+    if(top==-1){
+        printf("Stack Underflow\n");
+        exit(1);
+    }
+    c=stack[top];
+    top=top-1;
+    return c;
+}
+int isEmpty(){
+    if(top==-1)
     return 1;
     else
     return 0;
 }
-int isEmpty(struct stack* ptr){
-    if(ptr->top==-1){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-int isFull(struct stack* ptr){
-    if(ptr->top==ptr->size-1){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-void push(struct stack*ptr, char val){
-    if(isFull(ptr)){
-        printf("Stack overflow! \n");
-    }
-    else{
-        ptr->top++;
-        ptr->arr[ptr->top]=val;
-    }
-}
-int pop(struct stack*ptr){
-    if(isEmpty(ptr)){
-        printf("Stack Underflow! \n");
-    }
-    else{
-        int val=ptr->arr[ptr->top];
-        ptr->top--;
-        return val;
-    }
-}
-char * infixToPostfix(char * infix){
-    struct stack *sp=(struct stack*)malloc(sizeof(struct stack));
-    sp->size=100;
-    sp->top=-1;
-    sp->arr = (char *) malloc(sp->size * sizeof(char));
-    char * postfix=(char *)malloc((strlen(infix)+1)*sizeof(char));
-    int i=0; //track infix traversal
-    int j=0; //track postfix traversal
-    while(infix[i]!='\0'){
-        if(!isOperator(infix[i])){
-            postfix[j]=infix[i];
-            j++;
-            i++;
-        }
-        else{
-            if(precedence(infix[i])>precedence(stackTop(sp))){
-                push(sp, infix[i]);
-                i++;
-            }
-            else{
-                postfix[j]=pop(sp);
-                j++;
+
+ void inToPost(){
+    int i,j=0;
+    char next;
+    char symbol;
+    for(i=0;i<strlen(infix);i++){
+        symbol=infix[i];
+        //if symbol is not a whitespace or tab
+        if(!space(symbol)){
+            switch(symbol){
+                case '(':
+                push(symbol);
+                break;
+                case ')':
+                while((next=pop())!='('){
+                    postfix[j++]=next;
+                }
+                break;
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '^':
+                while(!isEmpty()&&precedence(stack[top])>=precedence(symbol)){
+                    postfix[j++]=pop();
+                }
+                push(symbol);
+                break;
+                default:
+                postfix[j++]=symbol;
             }
         }
     }
-    while(!isEmpty(sp)){
-        postfix[j]=pop(sp);
-        j++;
+    while(!isEmpty()){
+        postfix[j++]=pop();
     }
     postfix[j]='\0';
-    return postfix;
-}
+ }
+ postEval(){
+    int i;
+    int a,b;
+    for(i=0;i<strlen(postfix);i++){
+        if(postfix[i]>='0'&&postfix[i]<='9'){
+            push(postfix[i]-'0');
+        }
+        else{
+            a=pop();
+            b=pop();
+            switch(postfix[i]){
+                case'+':
+                push(b+a);
+                break;
+                case'-':
+                push(b-a);
+                break;
+                case'*':
+                push(b*a);
+                break;
+                case'/':
+                push(b/a);
+                break;
+                case'^':
+                push(pow(b,a));
+                break;
+            }
+        }
+    }
+    return pop();
+ }
 int main(){
-    char * infix ="x-y/(z-k)*d";
-    printf("postfix is %s",infixToPostfix(infix));
+    printf("Enter infix expression: ");
+    gets(infix);
+    inToPost();
+    int result=postEval();
+    print();
+    printf("The result after evaluation is %d\n",result);
     return 0;
 }
 
